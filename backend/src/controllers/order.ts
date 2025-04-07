61f34273f140ed97e3adf6f82a166ec270b5aed8
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-04-07 15:03:08 +0300

    fix order controllers

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,32 +1,31 @@
-import {  Request, Response } from "express";
+import { Request, Response } from "express";
 import Product from "../models/product";
 import { faker } from "@faker-js/faker";
 import BadRequestError from "../errors/bad-request-error";
 
-
-export const orderProduct = async (
-  req: Request,
-  res: Response
-) => {
+export const orderProduct = async (req: Request, res: Response) => {
   const { total, items } = req.body;
 
-  const products = await Product.find({ _id: { $in: items } });
+  let products;
 
-  const filteredProducts = products.filter((product) => product.price !== null);
-
+  try {
+    products = await Product.find({ _id: { $in: items } });
+  } catch (err) {
+    throw new BadRequestError("Data base error");
+  }
   if (products.length === 0) {
     throw new BadRequestError("Empty cart");
   }
 
-  if (filteredProducts.length !== items.length) {
-    throw new BadRequestError("Wrong order");
-  }
-
+  const filteredProducts = products.filter((product) => product.price !== null);
   const totalSum = filteredProducts.reduce(
     (acc, product) => acc + product.price,
     0
   );
 
+  if (filteredProducts.length !== items.length) {
+    throw new BadRequestError("Wrong order");
+  }
   if (totalSum !== total) {
     throw new BadRequestError("Wrong total sum");
   }
